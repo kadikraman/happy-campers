a3e854c58b4b848f0a8dd2e3456cc85cafc35855
Kadi Kraman <kadi.kraman@red-badger.com>
2017-02-04 09:45:08 +0000

    Move Vue.use(Vuex) to main where it belongs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 
 import createStore from './store';
 
-const store = createStore(Vue);
+Vue.use(Vuex);
+const store = createStore();
 
 /* eslint-disable no-new */
 new Vue({
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
-import Vuex, { Store } from 'vuex';
+import { Store } from 'vuex';
 import initialGrid from './initialGrid';
 import mutations from './mutations';
 import { TOGGLE } from './mutationTypes';
 
-export default (Vue) => {
-  Vue.use(Vuex);
-  return new Store({
-    state: {
-      grid: initialGrid,
-    },
-    mutations,
-    actions: {
-      [TOGGLE]: ({ commit }, payload) => commit(TOGGLE, payload),
-    },
-  });
-};
+export default () => new Store({
+  state: {
+    grid: initialGrid,
+  },
+  mutations,
+  actions: {
+    [TOGGLE]: ({ commit }, payload) => commit(TOGGLE, payload),
+  },
+});
